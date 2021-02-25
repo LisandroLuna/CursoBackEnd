9fe43b5afda0a482b8d6ae0477103e03d736872d
Lisandro Luna <lisandro_luna@ymail.com>
2021-02-24 21:27:20 -0300

    Clase 13 - desafio.

---
+++ Clase 13/data/message.mjs
@@ -0,0 +1,11 @@
+let messages = [
+    { author: "Juan", text: "¡Hola! ¿Que tal?" },
+    { author: "Pedro", text: "¡Muy bien! ¿Y vos?" },
+    { author: "Ana", text: "¡Genial!" }
+];
+
+function addMess(data){
+    messages = [...messages, data]
+}
+
+export {messages, addMess}
